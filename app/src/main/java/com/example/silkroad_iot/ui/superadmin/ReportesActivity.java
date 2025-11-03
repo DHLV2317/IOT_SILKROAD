9e104850673d7f40d48c67a9881e6025d9a490b2
PJ-JP <a20213805@pucp.edu.pe>
2025-11-03 18:01:37 -0500

    LineChart de DetallesReporteActivity.java

--- app/src/main/java/com/example/silkroad_iot/ui/superadmin/ReportesActivity.java
+++ app/src/main/java/com/example/silkroad_iot/ui/superadmin/ReportesActivity.java
@@ -12,12 +12,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.silkroad_iot.R;
+import com.example.silkroad_iot.data.EmpresaFb;
 import com.example.silkroad_iot.databinding.ActivitySuperadminReportesBinding;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,7 +31,8 @@ import java.util.List;
 public class ReportesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ActivitySuperadminReportesBinding binding;
-    private final List<ReporteItem> data = new ArrayList<>();
+    //private final List<ReporteItem> data = new ArrayList<>();
+    private final List<EmpresaFb> data = new ArrayList<>();
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
@@ -72,15 +75,14 @@ public class ReportesActivity extends AppCompatActivity implements NavigationVie
 
     private void cargarLista() {
         data.clear();
-        db.collection("reportes")
+        db.collection("empresas")
                 .get()
                 .addOnSuccessListener(snap -> {
                     for (QueryDocumentSnapshot d : snap) {
-                        ReporteItem r = new ReporteItem();
-                        r.id = d.getId();
-                        r.titulo = String.valueOf(d.get("titulo"));
-                        r.descripcion = String.valueOf(d.get("descripcion"));
-                        data.add(r);
+                        if(d.contains("correo")){
+                            EmpresaFb e = d.toObject(EmpresaFb.class);;
+                            data.add(e);
+                        }
                     }
                     adapter.notifyDataSetChanged();
                 });
@@ -100,26 +102,36 @@ public class ReportesActivity extends AppCompatActivity implements NavigationVie
     }
 
     private static class ReportesAdapter extends RecyclerView.Adapter<ReportesAdapter.VH> {
-        private final List<ReporteItem> items;
-        ReportesAdapter(List<ReporteItem> items){ this.items = items; }
+        //private final List<ReporteItem> items;
+        private final List<EmpresaFb> items;
+        //ReportesAdapter(List<ReporteItem> items){ this.items = items; }
+        ReportesAdapter(List<EmpresaFb> items){ this.items = items; }
         @NonNull @Override public VH onCreateViewHolder(@NonNull ViewGroup p, int v) {
-            View view = LayoutInflater.from(p.getContext()).inflate(android.R.layout.simple_list_item_2, p, false);
+            View view = LayoutInflater.from(p.getContext()).inflate(R.layout.sp_administrador_rv, p, false);
             return new VH(view);
         }
         @Override public void onBindViewHolder(@NonNull VH h, int pos) {
-            ReporteItem r = items.get(pos);
-            h.t1.setText(r.titulo == null ? "(Sin título)" : r.titulo);
-            h.t2.setText(r.descripcion == null ? "" : r.descripcion);
-            h.itemView.setOnClickListener(v -> {
+            //ReporteItem r = items.get(pos);
+            EmpresaFb r = items.get(pos);
+            h.t1.setText(r.getNombre() == null ? "(Sin título)" : r.getNombre());
+            h.t2.setText(r.getEmail() == null ? "" : r.getEmail());
+            h.card.setOnClickListener(v -> {
                 Intent i = new Intent(v.getContext(), DetallesReporteActivity.class);
-                i.putExtra("titulo", r.titulo);
-                i.putExtra("descripcion", r.descripcion);
+                //i.putExtra("titulo", r.titulo);
+                //i.putExtra("descripcion", r.descripcion);
                 v.getContext().startActivity(i);
             });
         }
         @Override public int getItemCount(){ return items.size(); }
         static class VH extends RecyclerView.ViewHolder {
-            TextView t1, t2; VH(@NonNull View v){ super(v); t1=v.findViewById(android.R.id.text1); t2=v.findViewById(android.R.id.text2); }
+            TextView t1, t2;
+            CardView card;
+            VH(@NonNull View v){
+                super(v);
+                t1=v.findViewById(R.id.textView1);
+                t2=v.findViewById(R.id.textView2);
+                card=v.findViewById(R.id.cardView1);
+            }
         }
     }
 }
